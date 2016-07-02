a22af29e9b13b587f0a680c960168a7ffbb8f639
AaronChelvan <aaron.chelvan@outlook.com>
2016-07-02 18:40:36 +1000

    Commit6
    - The player can now create their own word bank to play from
    - Replaced some 'while' loops with 'for' loops, where possible
    - Added some more comments to make the code easier to understand

--- hangman.c
+++ hangman.c
@@ -1,6 +1,6 @@
 // Aaron Chelvan
 // Started on: 27/04/16
-// Most recent update: 28/05/16
+// Most recent update: 02/07/16
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,45 +9,85 @@
 
 #define TRUE 1
 #define FALSE 0
+#define MAX_WORD_LENGTH 50
 
-void printIntro (void);
+void printIntro (void); 
+//Prints the intro ASCII image
+
+int checkLowercase (char *inputString); 
+//Checks if a given string contains ONLY lowercase characters
+
 void printImage (int mistakes);
+//Prints an ASCII image of the stick figure being hanged
 
 int main (int argc, char * argv[]) {
+   printIntro();
+
+   //Create an array of strings - this is the word bank
+   printf("How many words in the word bank? (Enter an integer)\n");
+   int numOfWords = 0;
+   scanf("%d", &numOfWords);
+   char wordList[numOfWords][MAX_WORD_LENGTH]; 
+   printf("\n");
+
+   //Fill the word bank with words inputted by the user
+   int counter = 0;
+   while (counter < numOfWords) {
+      printf("What is word %d?\n", (counter + 1));
+      printf("(Maximum permitted word length is 50 letters)\n");
+      printf("(Lowercase letters ONLY)\n");
+      char inputString[MAX_WORD_LENGTH];
+      scanf("%s", inputString);
+
+      //Check if the inputted string contains only lowercase letters
+      if (checkLowercase(inputString) == TRUE) {
+         //Copy the inputted string into the word bank
+         int counter2 = 0;
+         for (counter2 = 0; counter2 < MAX_WORD_LENGTH; counter2++) {
+            wordList[counter][counter2] = inputString[counter2];
+         }
+         counter++;
+         printf("\n");
+      } else {
+         printf("Invalid input. Try again.\n");
+         printf("\n");
+         continue;
+      }
+   }
+
    //Randomly choose a word from the list
    srand(time(NULL));
-   int wordNum = rand()%4;
-   char *wordList[10];
-   wordList[0] = "dog"; 
-   wordList[1] = "cat";
-   wordList[2] = "giraffe";
-   wordList[3] = "elephant";
-   wordList[4] = "hippopotamus";
-   wordList[5] = "jaguar";
-   wordList[6] = "rhinoceros";
-   wordList[7] = "crocodile";
-   char *word = wordList[wordNum];
+   int wordNum = rand()%numOfWords;
+   char *word = &wordList[wordNum][0];
 
    int solvedStatus = FALSE;
    int mistakes = 0;
    
+   //solvedArray contains a number for each letter in the word
+   //0 = that letter has not yet been guessed by the user
+   //1 = that letter has been guessed by the user
+   //E.g. If the word is 'apple' and the user has guessed
+   //'p' and 'a' so far, solvedArray would be: [1,1,1,0,0]
    int solvedArray[(int)strlen(word)];
    //Fill solvedArray with zeroes
-   int counter = 0;
-   while(counter < strlen(word)) {
+   for (counter = 0; counter < strlen(word); counter++) {
       solvedArray[counter] = 0;
-      counter++;
    }
 
+   //checkArray contains a number for each letter in the word 
+   //0 = the current letter being guessed is not this letter
+   //1 = the current letter being guessed is this letter
+   //E.g. If the word is 'apple' and the user has guessed
+   //'a' on the previous turn but 'p' this turn, 
+   //checkArray would be: [0,1,1,0,0] (only takes into account
+   //the current guess)
    int checkArray[(int)strlen(word)];
    //Fill checkArray with zeroes
-   counter = 0;
-   while(counter < strlen(word)) {
+   for (counter = 0; counter < strlen(word); counter++) {
       checkArray[counter] = 0;
-      counter++;
    }
 
-   printIntro();
+   printf("Let's Start!\n");
    
    while ((solvedStatus == FALSE) && (mistakes < 7)) {
       printf("=========================================\n");
@@ -59,15 +99,13 @@ int main (int argc, char * argv[]) {
       
       int unsolvedLetters = 0;
       //Check if 'letter' is in 'word'
-      counter = 0;
-      while (counter < strlen(word)) {
+      for (counter = 0; counter < strlen(word); counter++) {
          if (word[counter] == letter) {
             checkArray[counter] = 1;
          } else {
             checkArray[counter] = 0;
             unsolvedLetters++;
          }
-         counter++;
       }
 
       if (unsolvedLetters == strlen(word)) {
@@ -76,9 +114,6 @@ int main (int argc, char * argv[]) {
       printf("Number of mistakes: %d\n", mistakes);
       printImage(mistakes);
 
-      //solvedArray contains a number for each letter in 'word'
-      //0 = that letter has not yet been guessed by the user
-      //1 = that letter has been guessed by the user
       counter = 0;
       while (counter < strlen(word)) {
          if ((checkArray[counter] == 0) &&
@@ -91,21 +126,17 @@ int main (int argc, char * argv[]) {
       }
       
       //Fill checkArray with zeroes
-      counter = 0;
-      while(counter < strlen(word)) {
+      for (counter = 0; counter < strlen(word); counter++) {
          checkArray[counter] = 0;
-         counter++;
       }
 
       //Count how many letters have been found
-      int solvedLetters = 0;
-      counter = 0;
-      while (counter < strlen(word)) {
+      int solvedLetters = 0;  
+      for (counter = 0; counter < strlen(word); counter++) {
          if (solvedArray[counter] == 1) {
             solvedLetters++;
          }
-         counter++;
-      }      
+      }
 
       //Check if all letters have been found
       if (solvedLetters == strlen(word)) {
@@ -114,20 +145,19 @@ int main (int argc, char * argv[]) {
 
       //Print out the word
       //'_' represent letters that have not yet been guessed
-      counter = 0;
-      while (counter < strlen(word)) {
+      for (counter = 0; counter < strlen(word); counter++) {
          if (solvedArray[counter] == 0) {
             printf("_");
          } else if (solvedArray[counter] == 1) {
             printf("%c", word[counter]);
          }
-         counter++;
       }
       printf("\n");  
       printf("\n");   
    }
 
    if (mistakes == 7) {
+      printf("Correct word was: %s\n", word);
       printf("YOU LOSE!\n\n");
    } else {
       printf("YOU WIN!\n\n");
@@ -143,9 +173,23 @@ void printIntro (void) {
    printf("|  | |__| | \\ | | __  | \\  / | |__| | \\ |\n");
    printf("|  | |  | |  \\| |___| |  \\/  | |  | |  \\|\n");
    printf("=========================================\n");
+   printf("By Aaron Chelvan\n");
    printf("\n");
    printf("Guess the word in less than 7 mistakes to win.\n");
    printf("\n");
+}
+
+int checkLowercase (char *inputString) {
+   int counter = 0;
+   while (counter < strlen(inputString)) {
+      //If the character is not a lowercase letter, return 'false' 
+      if (!('a' <= inputString[counter] && inputString[counter] <= 'z')) {
+         return FALSE;
+      } else {
+         counter++;
+      }
+   }
+   return TRUE;
 }
 
 void printImage (int mistakes) {
